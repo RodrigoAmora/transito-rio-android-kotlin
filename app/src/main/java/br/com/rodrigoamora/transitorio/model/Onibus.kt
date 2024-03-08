7df75b2bc97108705316c78b400b062725108b2c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-03-07 22:43:10 -0300

    Criando a classe de modelo

---
+++ app/src/main/java/br/com/rodrigoamora/transitorio/model/Onibus.kt
@@ -0,0 +1,9 @@
+package br.com.rodrigoamora.transitorio.model
+
+data class Onibus (
+    var ordem: String,
+    var latitude: String,
+    var longitude: String,
+    var linha: String,
+    var datahoraenvio: String
+)
