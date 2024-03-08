098bfcd2f3f947f60208f3967521d3364ba64b4b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-03-07 22:27:13 -0300

    Atualizando dependências

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,27 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //AppCompat
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    //MaterialDesign
     implementation("com.google.android.material:material:1.11.0")
+
+    //ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    //Navigation Fragment
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    //Test
+
+    //jUnit
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    //Espresso
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
