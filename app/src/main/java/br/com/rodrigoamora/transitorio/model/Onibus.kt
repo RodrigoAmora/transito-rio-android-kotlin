489f7946b4c66c6c6c2e4b17af8f279fb9d81397
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-03-09 22:41:55 -0300

    Adicionando a anotação @SerializedName

--- app/src/main/java/br/com/rodrigoamora/transitorio/model/Onibus.kt
+++ app/src/main/java/br/com/rodrigoamora/transitorio/model/Onibus.kt
@@ -1,9 +1,20 @@
 package br.com.rodrigoamora.transitorio.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Onibus (
+    @SerializedName("id")
     var ordem: String,
+
+    @SerializedName("latitude")
     var latitude: String,
+
+    @SerializedName("longitude")
     var longitude: String,
+
+    @SerializedName("linha")
     var linha: String,
+
+    @SerializedName("datahoraenvio")
     var datahoraenvio: String
 )
