bb7fd9801658ef4d658f86ff78f9a71408a7e66b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-17 12:35:13 -0300

    Criando o método verificarHora

--- app/src/main/java/br/com/rodrigoamora/transitorio/repository/impl/OnibusRepositoryImpl.kt
+++ app/src/main/java/br/com/rodrigoamora/transitorio/repository/impl/OnibusRepositoryImpl.kt
@@ -7,6 +7,11 @@ import br.com.rodrigoamora.transitorio.model.Onibus
 import br.com.rodrigoamora.transitorio.network.webclient.OnibusWebClient
 import br.com.rodrigoamora.transitorio.repository.OnibusRepository
 import br.com.rodrigoamora.transitorio.repository.Resource
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.TimeZone
 
 class OnibusRepositoryImpl(
     private val onibusWebClient: OnibusWebClient
@@ -19,7 +24,9 @@ class OnibusRepositoryImpl(
 
         this.onibusWebClient.buscarOnibus(dataInicial,
             completion = { listaOnibus ->
-                mediator.value = Resource(listaOnibus)
+                listaOnibus?.let {
+                    mediator.value = Resource(verificarHora(it))
+                }
             },
             failure = {
             }
@@ -28,4 +35,20 @@ class OnibusRepositoryImpl(
         return this.mediator
     }
 
+    private fun verificarHora(listOnibus: List<Onibus>): List<Onibus> {
+        val noaLista = mutableListOf<Onibus>()
+        for (onibus in listOnibus) {
+            val triggerTime =
+                LocalDateTime.ofInstant(
+                    Instant.ofEpochMilli(onibus.datahoraenvio.toLong()),
+                    TimeZone.getTimeZone("America/Sao_Paulo").toZoneId()
+                ).plusHours(1)
+
+            val c = Duration.between(triggerTime, LocalDateTime.now()).toSeconds()
+            if (c <= 10) {
+                noaLista.add(onibus)
+            }
+        }
+        return noaLista.toList()
+    }
 }
