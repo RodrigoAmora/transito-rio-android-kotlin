7500be0aeb037485703247e8ddd73e9bf881eaa9
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-05-02 02:10:10 -0300

    Chamando o método enableEdgeToEdge

--- app/src/main/java/br/com/rodrigoamora/transitorio/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/rodrigoamora/transitorio/ui/activity/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
         this.checkPermissions()
     }
 
